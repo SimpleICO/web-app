f405c4a847a01819b1c82a740b1a6fe7e6cc1e6d
Gus <yo@soygus.com>
2018-07-25 14:42:15 -0600

    Adds QRCode generator to fixed-supply-widget

--- projects/contract-widgets/src/lib/fixed-supply/fixed-supply.component.ts
+++ projects/contract-widgets/src/lib/fixed-supply/fixed-supply.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input, ViewChild, ElementRef } from '@angular/core';
+
+declare var require: any
+
+const qrcode = require('qrcode-generator')
+const clipboard = require('clipboard')
 
 @Component({
   selector: 'fixed-supply-widget',
@@ -8,9 +13,21 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class FixedSupplyComponent implements OnInit {
 
+  QR: any
+
+  @ViewChild('qrCode') set el(el: ElementRef) {
+    this.QR = el
+  }
+
+  @Input() contract: any
+
   constructor() { }
 
   ngOnInit() {
+    let qr = qrcode(0, 'H')
+    qr.addData(this.contract.getAddress())
+    qr.make()
+    this.QR.nativeElement.innerHTML = qr.createImgTag(6, 2)
   }
 
 }
