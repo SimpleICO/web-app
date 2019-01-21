c2b6e70ba103445437db2e0e5c560f44f198a751
Gus <yo@soygus.com>
2019-01-21 15:53:21 -0600

    Makes routes to scroll to top

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -84,7 +84,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      // scrollPositionRestoration: 'top', anchorScrolling: 'enabled',
+      scrollPositionRestoration: 'top', anchorScrolling: 'enabled',
     })
   ],
   exports: [RouterModule]
