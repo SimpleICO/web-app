97fe4a5fe68305b435dfab5c5edb5fba829b9483
Gus <yo@soygus.com>
2019-01-24 18:56:11 -0600

    Adds tokens list to wallet. Works only on mainnet

--- src/app/wallet/container.component.ts
+++ src/app/wallet/container.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { WalletService } from '@decentralizedtechnologies/scui-lib';
+import { WalletService, Network } from '@decentralizedtechnologies/scui-lib';
 import { SharedService } from '@service/shared.service';
 import { EthereumService } from '@decentralizedtechnologies/scui-lib';
+import { HttpClient } from '@angular/common/http';
+import { DetailedERC20Deployment } from '@factory/detailed-erc20.deployment';
 
 @Component({
   selector: 'app-container',
@@ -10,12 +12,23 @@ import { EthereumService } from '@decentralizedtechnologies/scui-lib';
 })
 export class ContainerComponent implements OnInit {
 
+  tokens = []
+  DetailedERC20Deployment = DetailedERC20Deployment._type
+
   constructor(
     public wallet: WalletService,
     public eth: EthereumService,
+    private http: HttpClient,
     public shared: SharedService) { }
 
   ngOnInit() {
+    if (this.wallet.network === Network.mainnet) {
+      this.http.get(`http://api.ethplorer.io/getAddressInfo/${this.wallet.getAddress()}?apiKey=freekey`)
+        .toPromise()
+        .then((info: any) => {
+          this.tokens = info.tokens
+        }).catch(error => console.error(error))
+    }
   }
 
 }
